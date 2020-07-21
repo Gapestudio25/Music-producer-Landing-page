7e3f8a9d70a9001a70d8403afa75cfa69e214d44
Arian Garcia <arianmanuel75@gmail.com>
2020-07-21 18:58:28 -0400

    Contact card

--- src/components/contactUs/index.js
+++ src/components/contactUs/index.js
@@ -1,17 +1,77 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
+import {
+  makeStyles,
+  CssBaseline,
+  Container,
+  Typography,
+  Grid,
+  Card,
+  CardMedia,
+  CardContent,
+  Divider,
+} from '@material-ui/core';
+import {Phone, Email, LocationOn} from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
-  contactUs: {
-    height: '100vh',
-  },
+  // contactUs: {
+  //   height: '100vh',
+  // },
   heroContent: {
     padding: theme.spacing(8, 0, 6),
   },
+  card: {
+    maxWidth: 500,
+    margin: "auto",
+    boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)",
+    paddingBottom: `${theme.spacing.unit * 2}px`
+  },
+  media: {
+    paddingTop: "56.25%"
+  },
+  content: {
+    textAlign: "left",
+    padding: `${theme.spacing.unit * 5}px ${theme.spacing.unit * 5}px 0`
+  },
+  contactTypeIcon: {
+    marginRight: '20px',
+  },
+  divider: {
+    margin: `${theme.spacing.unit * 3}px 0`
+  },
 }));
+
+const phones = [
+  {
+    type: 'Mobile',
+    number: '(999) 999-9999',
+  },
+  {
+    type: 'Home',
+    number: '(000) 000-0000',
+  },
+];
+
+const emails = [
+  {
+    type: 'Personal',
+    mail: 'JohnDoe@example.com',
+  },
+  {
+    type: 'Company',
+    mail: 'JohnDoe@ecompany.com',
+  },
+];
+
+const locations = [
+  {
+    type: 'Home',
+    direction: '1419 Westwood Blvd, LA',
+  },
+  {
+    type: 'Office',
+    direction: '1419 Westwood Blvd, LA',
+  },
+];
 
 const ContactUs = () => {
   const classes = useStyles();
@@ -24,6 +84,82 @@ const ContactUs = () => {
           <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
             Contact Us
           </Typography>
+        </Container>
+        <Container maxWidth="md" component="main">
+          <Grid Container direction="row" alignItems="center" justify="center">
+            <Grid item>
+              <Card className={classes.card}>
+                <CardMedia
+                  className={classes.media}
+                  image={
+                    "https://image.freepik.com/free-photo/river-foggy-mountains-landscape_1204-511.jpg"
+                  }
+                />
+                <CardContent className={classes.content}>
+                  <Grid container direction="row" alignItems="stretch">
+                    <Grid item xs={1} className={classes.contactTypeIcon}>
+                      <Phone/>
+                    </Grid>
+                    <Grid item container direction="column" xs={10} spacing={2}>
+                      {phones.map((phone) => (
+                        <Grid item>
+                          <Typography>
+                            {phone.number}
+                          </Typography>
+
+                          <Typography variant={"caption"} color="secondary" className={classes.test}>
+                            {phone.type}
+                          </Typography>
+                        </Grid>
+                      ))}
+                    </Grid>
+                  </Grid>
+
+                  <Divider className={classes.divider} light />
+                  
+                  <Grid container direction="row" alignItems="stretch">
+                    <Grid item xs={1} className={classes.contactTypeIcon}>
+                      <Email/>
+                    </Grid>
+                    <Grid item container direction="column" xs={10} spacing={2}>
+                      {emails.map((email) => (
+                        <Grid item>
+                          <Typography>
+                            {email.mail}
+                          </Typography>
+
+                          <Typography variant={"caption"} color="secondary" className={classes.test}>
+                            {email.type}
+                          </Typography>
+                        </Grid>
+                      ))}
+                    </Grid>
+                  </Grid>
+
+                  <Divider className={classes.divider} light />
+
+                  <Grid container direction="row" alignItems="stretch">
+                    <Grid item xs={1} className={classes.contactTypeIcon}>
+                      <LocationOn/>
+                    </Grid>
+                    <Grid item container direction="column" xs={10} spacing={2}>
+                      {locations.map((location) => (
+                        <Grid item>
+                          <Typography>
+                            {location.direction}
+                          </Typography>
+
+                          <Typography variant={"caption"} color="secondary" className={classes.test}>
+                            {location.type}
+                          </Typography>
+                        </Grid>
+                      ))}
+                    </Grid>
+                  </Grid>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
         </Container>
       </div>
     </React.Fragment>
--- src/components/playlist/index.js
+++ src/components/playlist/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
+import {
+  makeStyles,
+  CssBaseline,
+  Container,
+  Typography,
+  Grid,
+} from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
-  playlist: {
-    height: '100vh',
-  },
   heroContent: {
     padding: theme.spacing(8, 0, 6),
   },
   reproducer: {
     width: '100%',
-    maxWidth: '1024px',
     height: '80vh',
   }
 }));
@@ -22,19 +21,23 @@ const Playlist = () => {
   const classes = useStyles();
 
   return (
-    <React.Fragment>
-      <div id="playlist" className={classes.playlist}>
+    <div id="playlist" className={classes.playlist}>
+      <Container maxWidth="xl" component="main" className={classes.heroContent}>  
         <CssBaseline />
-        <Container maxWidth="sm" component="main" className={classes.heroContent}>
-          <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-            Playlist
-          </Typography>
-        </Container>
-        <Container maxWidth="md" component="main">
-          <iframe src="https://player.beatstars.com/?storeId=107684" className={classes.reproducer} />
-        </Container>
-      </div>
-    </React.Fragment>
+        <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+          Playlist
+        </Typography>
+      </Container>
+      <Container maxWidth="xl" component="main">
+        <Grid container justify="center" alignItems="center">
+          <iframe
+            src="https://player.beatstars.com/?storeId=107684"
+            title="Music Reproducer"
+            className={classes.reproducer}
+          />
+        </Grid>
+      </Container>
+    </div>
   );
 };
 
